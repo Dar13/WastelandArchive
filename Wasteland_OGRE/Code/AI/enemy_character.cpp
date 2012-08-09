c43dc36a5a67b153ac4b6d75864fb33507dd10a3
Neil Moore <dar13.moore@gmail.com>
2012-08-09 14:30:59 -0400

    Added files for the enemy character. Barebones, but they compile.

---
+++ Wasteland_OGRE/Code/AI/enemy_character.cpp
@@ -0,0 +1,3 @@
+#include "StdAfx.h"
+
+#include "enemy_character.h"
---
+++ Wasteland_OGRE/Code/AI/enemy_character.h
@@ -0,0 +1,11 @@
+#include "StdAfx.h"
+
+#ifndef _ENEMY_CHARACTER_H_
+#define _ENEMY_CHARACTER_H_
+
+class Enemy_Character
+{
+
+};
+
+#endif
